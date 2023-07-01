5e83d3b4cf0ba0ceea3b4f349bc99487b42f25cc
MihdarAbbasher <mihdar.ph@gmail.com>
2023-07-01 18:11:29 +0200

    octa unsigned

---
+++ print_unsigned.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_unsigned - prints an unsigned integer
+ * @item: Integer
+ * Return: int
+ */
+
+int print_unsigned(va_list item)
+{
+	unsigned int n = va_arg(item, int);
+	long int count = 1, num = n;
+
+	while (n > 9)
+		n /= 10,
+		count++;
+
+	print_num_rec(num);
+	return (count);
+}
